8174bfa77a438f565d7312a3a2c3ee940aae1858
Ethan <git@ethanlibs.co>
2022-05-06 19:23:13 -0400

    gaming

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,21 +1,7 @@
 pluginManagement {
     repositories {
-        mavenLocal()
         gradlePluginPortal()
-        mavenCentral()
-
-        maven("https://jitpack.io/")
-        maven("https://maven.architectury.dev/")
-        maven("https://maven.fabricmc.net")
-        maven("https://maven.minecraftforge.net")
-        maven("https://repo.sk1er.club/repository/maven-public/")
-    }
-    resolutionStrategy {
-        eachPlugin {
-            when (requested.id.id) {
-                "gg.essential.loom" -> useModule("gg.essential:architectury-loom:${requested.version}")
-            }
-        }
+        maven("https://repo.woverflow.cc/")
     }
 }
 
